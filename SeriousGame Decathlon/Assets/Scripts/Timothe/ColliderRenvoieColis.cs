148f8158e9cd6ad7d4907cd1885b8ac8e53f0fc0
LinKazer <t.lafranche@rubika-edu.com>
2019-06-14 10:51:23 +0200

    Renvoie Colis OP

---
+++ SeriousGame Decathlon/Assets/Scripts/Timothe/ColliderRenvoieColis.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderRenvoieColis : MonoBehaviour
+{
+    public ColisManager manage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<ColisScript>() != null && !collision.gameObject.GetComponent<ColisScript>().doesEntrance)
+        {
+            manage.RenvoieColis(collision.gameObject);
+        }
+    }
+}
