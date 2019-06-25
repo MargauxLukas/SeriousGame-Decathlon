6203861376de0a45b8fc4215b5bd6e1860e4f7c0
Timothe <linkazer@gmail.com>
2019-06-25 14:28:40 +0200

    Résolution bug ouverture Ecran

--- SeriousGame Decathlon/Assets/Scripts/Ecran/BigMonitor.cs
+++ SeriousGame Decathlon/Assets/Scripts/Ecran/BigMonitor.cs
@@ -11,41 +11,47 @@ public class BigMonitor : MonoBehaviour
 
     private bool isOpen         = false;                         //Le grand écran est-il ouvert ?
     public  bool monitorOpening = false;                         //Le grand écran est-il entrain de s'ouvrir ?
+    private bool monitorClosing = false;
 
     private float        startPos;                              //Position de départ du doigt
     private float          endPos;                              //Position de fin du doigt
     private float swipeDifference;                              //Différence entre startPos et endPos
 
     private void Update()
-    {      
-        if (Input.touchCount > 0)
+    {
+        if (monitorClosing)
         {
-            Touch touch = Input.GetTouch(0);
-
-            if(touch.phase == TouchPhase.Began) { startPos = touch.position.x; }
-            if(touch.phase == TouchPhase.Ended) { endPos   = touch.position.x; }
-            else                                { return                     ; }
-
-            swipeDifference = Mathf.Abs(startPos - endPos);
-            //Debug.Log("StartPos : " + startPos + "/ endPos : " + endPos + " /diff : " + swipeDifference);
-        }
-
-        if (monitorOpening){openBigMonitor();}                                                            //Condition pour éviter de l'ouvrir en boucle et consommé
-
-        if((endPos > startPos) && isOpen && swipeDifference>100f)                                         //Swipe vers la droite
-        {
-            transform.position = Vector2.MoveTowards(transform.position, initialPosition, 1f);
-
-            if(Vector2.Distance(transform.position, initialPosition) <= 0.2f)
+            if (Input.touchCount > 0)
             {
-                isOpen = false;
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began) { startPos = touch.position.x; }
+                if (touch.phase == TouchPhase.Ended) { endPos = touch.position.x; }
+                else { return; }
+
+                swipeDifference = Mathf.Abs(startPos - endPos);
+                //Debug.Log("StartPos : " + startPos + "/ endPos : " + endPos + " /diff : " + swipeDifference);
             }
         }
-        else if ((endPos < startPos) && isOpen && swipeDifference > 100f)                                 //Swipe vers la gauche
-        {          
-            return;
+
+        if (monitorOpening) { openBigMonitor(); }                                                            //Condition pour éviter de l'ouvrir en boucle et consommé
+        else
+        {
+            if ((endPos > startPos) && isOpen && swipeDifference > 100f)                                         //Swipe vers la droite
+            {
+                transform.position = Vector2.MoveTowards(transform.position, initialPosition, 1f);
+
+                if (Vector2.Distance(transform.position, initialPosition) <= 0.2f)
+                {
+                    isOpen = false;
+                }
+            }
+            else if ((endPos < startPos) && isOpen && swipeDifference > 100f)                                 //Swipe vers la gauche
+            {
+                return;
+            }
+            else { return; }
         }
-        else { return; }
     }
 
 
