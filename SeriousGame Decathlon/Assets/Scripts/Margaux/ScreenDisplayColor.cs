66036d3d7f6f7a012cdccffcd1b8bfaa19b19f20
AlexC <aoyama_masaya@hotmail.fr>
2019-07-04 10:53:37 +0100

    Add : Lamp on the Monitor

--- SeriousGame Decathlon/Assets/Scripts/Margaux/ScreenDisplayColor.cs
+++ SeriousGame Decathlon/Assets/Scripts/Margaux/ScreenDisplayColor.cs
@@ -5,29 +5,43 @@ using UnityEngine;
 public class ScreenDisplayColor : MonoBehaviour
 {
     public RecountTab recountTab;
+
     private SpriteRenderer screenColor;
+
+    public GameObject lamp;
+    private Animator lampAnimator;
 
     void Start()
     {
         screenColor = gameObject.GetComponent<SpriteRenderer>();
+        screenColor.enabled = false;
+        lampAnimator = lamp.GetComponent<Animator>();
+        lampAnimator.SetBool("hasAnomalie", false);
     }
 
     void Update()
     {
         if (recountTab.colis != null)
         {
+            screenColor.enabled = true;
             if (recountTab.colis.GetComponent<ColisScript>().hasBeenScannedByPistol && recountTab.colis.GetComponent<ColisScript>().colisScriptable.nbAnomalie > 0)
             {
                 screenColor.color = new Color32(192, 30, 41, 255);
+                lampAnimator.SetBool("hasAnomalie", true);
             }
             else if (recountTab.colis.GetComponent<ColisScript>().hasBeenScannedByPistol && recountTab.colis.GetComponent<ColisScript>().colisScriptable.nbAnomalie == 0)
             {
                 screenColor.color = new Color32(30, 192, 112, 255);
+                lampAnimator.SetBool("hasAnomalie", false);
             }
             else
             {
-                screenColor.color = new Color32(0, 198, 255, 255);
+                screenColor.enabled = false;
             }
+        }
+        else
+        {
+            screenColor.enabled = false;
         }
     }
 }
