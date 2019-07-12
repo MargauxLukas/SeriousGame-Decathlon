d100bb67af58332592cc137c743a1b9e8aa42d3b
Timothe <linkazer@gmail.com>
2019-07-12 10:54:45 +0200

    Prefab et Script affichage des anomalies en Reception

---
+++ SeriousGame Decathlon/Assets/Scripts/Timothe/Réception/ZoneAffichageAnomalieFiche.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoneAffichageAnomalieFiche : MonoBehaviour
+{
+    public Text zoneNombreAnomaliePresente;
+    public Text zoneAffichageAnomalie;
+}
