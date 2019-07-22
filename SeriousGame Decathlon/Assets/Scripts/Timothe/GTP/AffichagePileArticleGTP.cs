e093a9625b2bb27e0923cb58f65db990ee7dbabd
Timothe <linkazer@gmail.com>
2019-07-22 17:36:40 +0200

    Avancé proto GTP

---
+++ SeriousGame Decathlon/Assets/Scripts/Timothe/GTP/AffichagePileArticleGTP.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AffichagePileArticleGTP : MonoBehaviour
+{
+    public Colis currentColis;
+    private bool doesTouch;
+
+    public GameObject tasArticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentColis = Instantiate(currentColis);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            touchObject();
+
+            if(Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                doesTouch = false;
+            }
+
+            if(doesTouch)
+            {
+                if (!tasArticle.activeSelf)
+                {
+                    tasArticle.SetActive(true);
+                    tasArticle.GetComponent<TasArticleGTP>().OpenTasArticle(currentColis.listArticles);
+                }
+                else
+                {
+                    tasArticle.SetActive(false);
+                    currentColis.listArticles = tasArticle.GetComponent<TasArticleGTP>().CloseTasArticle();
+                }
+                doesTouch = false;
+            }
+        }
+    }
+
+    void touchObject()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject != null && gameObject != null && hit.collider.gameObject == gameObject && hit.collider.gameObject.name == gameObject.name)
+            {
+                doesTouch = true;
+            }
+        }
+    }
+}
