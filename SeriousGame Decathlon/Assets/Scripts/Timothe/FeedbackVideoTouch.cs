de66e81d910454d8f41584773a47a87e0fe78956
Timothe <linkazer@gmail.com>
2019-07-22 11:04:42 +0200

    Feedback pour la vidéo

---
+++ SeriousGame Decathlon/Assets/Scripts/Timothe/FeedbackVideoTouch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackVideoTouch : MonoBehaviour
+{
+    Color currentColor;
+    public float coef;
+    public float coefDeux;
+
+    public static FeedbackVideoTouch instance;
+
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(instance.gameObject);
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x, Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y, 0);
+                currentColor = Color.white;
+                currentColor.a = 1;
+                GetComponent<SpriteRenderer>().color = currentColor;
+                transform.localScale = Vector3.one * 0.1f;
+            }
+        }
+
+        if(currentColor.a >0)
+        {
+            currentColor.a -= Time.deltaTime * coef;
+            GetComponent<SpriteRenderer>().color = currentColor;
+        }
+        if(transform.localScale.x < 1f)
+        {
+            transform.localScale += Vector3.one * Time.deltaTime * coefDeux;
+        }
+    }
+}
