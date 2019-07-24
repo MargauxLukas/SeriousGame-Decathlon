fe03ed3a4575d8e45c46e81026bf94c95bc63e7e
AlexC <aoyama_masaya@hotmail.fr>
2019-07-24 11:51:41 +0100

    Add : Window Anomaly

--- SeriousGame Decathlon/Assets/Scripts/_GTP/Monitor.cs
+++ SeriousGame Decathlon/Assets/Scripts/_GTP/Monitor.cs
@@ -19,6 +19,8 @@ public class Monitor : MonoBehaviour
     public GameObject colisAPrelever1;
     public GameObject colisAPrelever2;
 
+    public GameObject anomalyWindow;
+
     public void Start()
     {
         nbText.text = "";
