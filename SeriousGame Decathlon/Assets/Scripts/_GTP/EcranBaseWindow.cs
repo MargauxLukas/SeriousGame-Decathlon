a8341437eeeb8c51a01550094a792058e29a1389
AlexC <aoyama_masaya@hotmail.fr>
2019-07-25 17:12:38 +0100

    Fixed : Ecran Pick TU Content not visible

--- SeriousGame Decathlon/Assets/Scripts/_GTP/EcranBaseWindow.cs
+++ SeriousGame Decathlon/Assets/Scripts/_GTP/EcranBaseWindow.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class EcranBaseWindow : MonoBehaviour
 {
     public GameObject anomalyWindow;
+    public GameObject PickTUContentWindow;
 
     public void AnomalyWindow()
     {
         gameObject   .SetActive(false);
-        anomalyWindow.SetActive(true);
+        anomalyWindow.SetActive(true );
+    }
+
+    public void PickTUWindow()
+    {
+        gameObject         .SetActive(false);
+        PickTUContentWindow.SetActive(true );
     }
 }
