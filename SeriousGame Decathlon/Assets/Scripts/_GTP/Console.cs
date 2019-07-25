270bfeded0ca7f38a90f862a5326826f515104c6
AlexC <aoyama_masaya@hotmail.fr>
2019-07-25 15:01:34 +0100

    Add : Set New Nombre Article

--- SeriousGame Decathlon/Assets/Scripts/_GTP/Console.cs
+++ SeriousGame Decathlon/Assets/Scripts/_GTP/Console.cs
@@ -9,6 +9,7 @@ public class Console : MonoBehaviour
     public Monitor monitor;
     public ConsoleMonitor cm;
 
+    public int emplacementConsole;
     public int nb = 0;
 
     public void Start()
@@ -38,7 +39,7 @@ public class Console : MonoBehaviour
     public void Valider() 
     {
         monitor.UpdateAffichage(monitor.nbMonitor + nb);
-        cm.UpdateAffichageConsole(nb);
+        cm.UpdateAffichageConsole(nb, emplacementConsole);
         nb = 0;
         UpdateAffichage();
     }
