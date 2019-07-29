ce6c25d5b0c17f49da9505547032364090992ebd
AlexC <aoyama_masaya@hotmail.fr>
2019-07-29 16:35:40 +0100

    Add : RemainingQuantity

---
+++ SeriousGame Decathlon/Assets/Scripts/_GTP/RemainingQuantityWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RemainingQuantityWindow : MonoBehaviour
+{
+    public TextMeshProUGUI remainingQuantitynb;
+    public TextMeshProUGUI tunb;
+    public TextMeshProUGUI expectednb;
+
+    public int articleNb;
+    public int nb = 0;
+
+    public void Affichage()
+    {
+        //Mettre a jour affichage
+        remainingQuantitynb.text = nb.ToString();
+    }
+
+    public void Moins()
+    {
+        nb--;
+        Affichage();
+    }
+
+    public void Plus()
+    {
+        nb++;
+        Affichage();
+    }
+
+    public void OK()
+    {
+        if(nb == articleNb)
+        {
+            //Bien
+        }
+        else
+        {
+            //Pas bien
+        }
+
+        gameObject.SetActive(false);
+    }
+}
