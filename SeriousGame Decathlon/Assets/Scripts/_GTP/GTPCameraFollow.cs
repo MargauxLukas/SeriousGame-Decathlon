552f6290cf5ffa4223cf5f5a19b49021045e8401
AlexC <aoyama_masaya@hotmail.fr>
2019-07-30 10:55:43 +0100

    Fixed : Camera position

--- SeriousGame Decathlon/Assets/Scripts/_GTP/GTPCameraFollow.cs
+++ SeriousGame Decathlon/Assets/Scripts/_GTP/GTPCameraFollow.cs
@@ -7,8 +7,8 @@ public class GTPCameraFollow : MonoBehaviour
     public GameObject player;
     public float offset = -0.5f;
 
-    private float maxY = 28f;
-    private float minY = 20f;
+    private float maxY = 6f;
+    private float minY = -4f;
 
     private float maxX = 20f;
     private float minX = -8f;
@@ -23,11 +23,11 @@ public class GTPCameraFollow : MonoBehaviour
         // Y déplacement
         if (player.transform.position.y < minY)
         {
-            transform.position = new Vector3(transform.position.x, 20f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, -4f, transform.position.z);
         }
         else if (player.transform.position.y > maxY)
         {
-            transform.position = new Vector3(transform.position.x, 28f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, 6f, transform.position.z);
         }
         else
         {
