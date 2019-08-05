899333ba09ae011dfd8c67d05a9851f9651b797f
Timothe <linkazer@gmail.com>
2019-08-05 16:38:57 +0200

    Choix du personnage finit

---
+++ SeriousGame Decathlon/Assets/Scripts/Timothe/cREATIONpERSONNAGE/AnimationPersoChoisit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPersoChoisit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GetComponent<Animator>()!=null && ManagerPersoChoisit.instance != null)
+        {
+            GetComponent<Animator>().runtimeAnimatorController = ManagerPersoChoisit.instance.animatorPersonnages[ManagerPersoChoisit.instance.Personnage];
+        }
+    }
+}
