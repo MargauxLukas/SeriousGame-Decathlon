b6f65f2f24496239bd540283bc1286ddea8ae0ac
Timothe <linkazer@gmail.com>
2019-08-13 14:50:37 +0200

    Ajouts des musiques du menu et du jeu

---
+++ SeriousGame Decathlon/Assets/Scripts/Timothe/TestSonsPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSonsPrincipal : MonoBehaviour
+{
+    public AudioClip musique;
+    public float coefSound = 1;
+
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.clip = musique;
+        source.volume = coefSound;
+        source.loop = true;
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
