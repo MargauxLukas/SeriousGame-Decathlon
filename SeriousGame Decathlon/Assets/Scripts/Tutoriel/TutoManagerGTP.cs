4d79444b94d518a42aab0673347569a782bb63c2
Margaux LUKAS <m.lukas@rubika-edu.com>
2019-08-13 13:20:43 +0200

    Nouvelle Scène TutoGTP && Managers

---
+++ SeriousGame Decathlon/Assets/Scripts/Tutoriel/TutoManagerGTP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoManagerGTP : MonoBehaviour
+{
+    public static TutoManagerGTP instance;
+
+    public DialogueManager dialogueManager;
+    public GameObjectsManagerRecep gameObjectsManager;
+
+    void Awake()
+    {
+        if (instance == null) { instance = this; }
+        else { Destroy(instance); }
+    }
+}
