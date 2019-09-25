548981382a260878edd260059dac73a8c11a5f0a
Timothe <t.lafranche@rubika-edu.com>
2019-09-25 15:28:05 +0200

    C réparé

---
+++ DecathlonMeca-Server/Assets/Scripts/ScreenSize.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1280, 800, false);
+    }
+
+}
